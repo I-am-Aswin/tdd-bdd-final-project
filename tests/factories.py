61b97e99fcba572d11ad055c30851cfd4239f578
I-am-Aswin <aswinsenthilkumar20@gmail.com>
2025-01-25 06:51:56 -0500

    TDD/BDD Project Version 1.0

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,29 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
+    name = FuzzyChoice( choices=[
+        "Mobile",
+        "Laptop",
+        "Desktop",
+        "Smart Watch",
+        "Tablet",
+        "Drone",
+        "NeckBand",
+        "TWS",
+        "Keyboard",
+        "Mouse",
+        "Speaker",
+        "Peripherals"
+    ])
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 50000.0, 2)
+    available = FuzzyChoice( choices=[True, False])
+    category = FuzzyChoice( choices=[
+        Category.UNKNOWN,
+        Category.CLOTHS,
+        Category.FOOD,
+        Category.HOUSEWARES,
+        Category.AUTOMOTIVE,
+        Category.TOOLS
+    ])
    ## Add code to create Fake Products 
